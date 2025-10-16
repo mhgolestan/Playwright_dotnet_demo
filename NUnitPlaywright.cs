1b702f7f91b091b389fd0658fb87ce9bc142b212
mhgolestan <golestan1369@gmail.com>
2025-10-16 09:47:24 +0200

    Refactor login test to use explicit locators and set default timeout

--- NUnitPlaywright.cs
+++ NUnitPlaywright.cs
@@ -15,10 +15,13 @@ public class NUnitPlaywright: PageTest
     [Test]
     public async Task Test1()
     {
-        await Page.ClickAsync("text=Login");
+        Page.SetDefaultTimeout(10); 
+        var loginLink = Page.Locator("text=Login");
+        await loginLink.ClickAsync();
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
-        await Page.ClickAsync("text=Log in");
+        var loginButton = Page.Locator("input" ,new PageLocatorOptions { HasTextString = "Log in" });
+        await loginButton.ClickAsync();
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
     }
 }
